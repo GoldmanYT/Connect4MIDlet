92c003b8f9668464580a9028611be686819dcfe3
GoldmanYT <89934420+GoldmanYT@users.noreply.github.com>
2026-05-15 14:14:34 +0300

    initial commit

---
+++ src/Connect4MIDlet.java
@@ -0,0 +1,21 @@
+import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+
+public class Connect4MIDlet extends MIDlet {
+	private Connect4Canvas gameCanvas;
+	private Display display;
+	
+	protected void destroyApp(boolean unconditional) {}
+	
+	protected void pauseApp() {}
+
+	protected void startApp() {
+		if (display == null) {
+			gameCanvas = new Connect4Canvas();
+		}
+		
+		display = Display.getDisplay(this);
+		display.setCurrent(gameCanvas);
+	}
+
+}
